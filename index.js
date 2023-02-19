136490738e155f113214e4b92bc5c8a8cc15fc5b
Md Abdur Rouf Likhon <mdabdurrouf.likhon@gmail.com>
2023-02-19 12:06:44 +0600

    random color

---
+++ index.js
@@ -0,0 +1,16 @@
+function randomColor() {
+  let color = [];
+  for (let i = 0; i < 3; i++) {
+    color.push(Math.floor(Math.random() * 256));
+    console.log(color);
+  }
+  return "rgb(" + color.join(", ") + ")";
+}
+
+document.addEventListener("mouseover", function () {
+  document.body.style.backgroundColor = randomColor();
+});
+
+document.querySelector("circle").addEventListener("mouseover", (event) => {
+  event.target.style.fill = randomColor();
+});
